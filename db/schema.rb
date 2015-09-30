ca8221db281494a344a5541e4891478fd5eef97c
Bad1993 <hogdubno@gmail.com>
2015-10-01 00:48:14 +0300

    created roles, relations with users

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925212634) do
+ActiveRecord::Schema.define(version: 20150930211234) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -33,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150925212634) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
